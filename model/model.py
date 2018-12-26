d6ada4fd9052e9cf58c55110f93f57525641e390
owenwei <821881341@qq.com>
2018-12-26 18:21:14 +0800

    update loss

--- model/model.py
+++ model/model.py
@@ -19,4 +19,5 @@ class MnistModel(BaseModel):
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+        return x
+        # return F.log_softmax(x, dim=1)
